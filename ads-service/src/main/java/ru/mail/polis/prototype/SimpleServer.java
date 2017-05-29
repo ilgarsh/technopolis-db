dfdf25b6c7becf798a832f8aea586b65f14260d5
olerom <mefiader@gmail.com>
2017-05-30 02:19:46 +0300

    Add very simple API method getAdvertisement: validate error with code of 1

--- ads-service/src/main/java/ru/mail/polis/prototype/SimpleServer.java
+++ ads-service/src/main/java/ru/mail/polis/prototype/SimpleServer.java
@@ -1,6 +1,5 @@
 package ru.mail.polis.prototype;
 
-import com.sun.corba.se.impl.orb.DataCollectorBase;
 import ru.mail.polis.prototype.database.Database;
 import ru.mail.polis.prototype.database.DatabaseImpl;
 
@@ -10,6 +9,7 @@ import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -62,25 +62,37 @@ public class SimpleServer {
             StringBuilder sb = new StringBuilder();
             while (-1 != (data = inputStream.read())) {
                 sb.append((char) data);
-
 //                for test
-                if ((char) data == '~') {
-                    break;
+                if ((char) data == '\n') {
+                    outputStream.write((dbWork(sb.toString()) + "\n").getBytes(Charset.forName("UTF-8")));
+                    sb = new StringBuilder();
                 }
             }
-
-            System.out.println(sb.toString());
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
 
-    private void handeAddition(String query) {
-        String[] tokens = query.split("/");
+    private int dbWork(String query) {
+        String[] tokens = query.split("\\?");
         try {
-            database.addAdvertiser(Long.valueOf(tokens[0]), Float.valueOf(tokens[1]));
+            if (!tokens[0].equals("getAdvertisement") && !tokens[0].equals("addAdvertisement")) {
+                return 1;
+            }
+            switch (tokens[0]) {
+                case "addAdvertisement":
+                    String[] shit = tokens[1].split("&");
+                    shit = shit[0].split("=");
+                    Long advId = Long.valueOf(shit[1]);
+                    database.addAdvertiser(advId, 13.5f);
+//                    validation kek
+                    break;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return 0;
     }
+
 }
+
